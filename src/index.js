2e658e25058d2e5c4c438d98c311fb7a44838070
Serg <tchernak.csg@gmail.com>
2022-03-05 18:15:36 +0300

    feat: complete morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrayExt = Array.from(expr);
+    let matrix = [];
+    let char = '';
+    let result = '';
+    
+    for (let i = 0; i < arrayExt.length/10; i++) {
+      matrix[i] = arrayExt.slice((i*10), (i*10) + 10);
+    }
+    
+    for (let k = 0; k < matrix.length; k++) {
+  
+      if (matrix[k][0] === '*')
+        result += ' ';
+  
+      else {
+        for (let j = 0; j < matrix[k].length; j++) {
+          if ((matrix[k][j] + matrix[k][j + 1]) === '10') {
+            char += '.';
+            j++;
+          } else if ((matrix[k][j] + matrix[k][j + 1]) === '11') {
+            char += '-';
+            j++;
+          }
+        }
+        result += MORSE_TABLE[char];
+        char = '';
+      }
+    }
+    return result;
 }
 
 module.exports = {
